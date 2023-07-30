5834b14467f56574d2988abb2e1c30a386fd1a5e
ericssendelima <ericssendelima@gmail.com>
2023-07-29 21:05:45 -0300

    Updates

---
+++ precache-manifest.019dc62d820d7e9307cdb21a014d9268.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "bab17db6a79c9ade716e",
+    "url": "/projeto-mega/static/css/main.c1151a30.chunk.css"
+  },
+  {
+    "revision": "bab17db6a79c9ade716e",
+    "url": "/projeto-mega/static/js/main.bab17db6.chunk.js"
+  },
+  {
+    "revision": "444f0bc38a785630c6eb",
+    "url": "/projeto-mega/static/js/1.444f0bc3.chunk.js"
+  },
+  {
+    "revision": "33adf687ce9139046b38",
+    "url": "/projeto-mega/static/js/2.33adf687.chunk.js"
+  },
+  {
+    "revision": "66ae0160c1fae6926c6d",
+    "url": "/projeto-mega/static/js/runtime~main.66ae0160.js"
+  },
+  {
+    "revision": "7ecc6df0f6ea432317406beaa3c8c9d5",
+    "url": "/projeto-mega/index.html"
+  }
+];
